7ceb0e80142ff83837b451007a9807243ee97e81
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-11-20 19:53:14 +0100

    Create GoogleAnalyticsObject.js

---
+++ GoogleAnalyticsObject.js
@@ -0,0 +1,15 @@
+(function(){
+var w=window,i=w.GoogleAnalyticsObject='ga',
+d=w.document,
+t='script',	
+e=d.createElement(t),
+s=d.getElementsByTagName(t)[0],
+a=(e.async=1,e.src='https://www.google-analytics.com/analytics.js');
+w[i]=w[i]||function(){
+ var o=w,p=i;
+ (o[p].q=o[p].q||[]).push(w,d,t,a,i,e,s);
+};
+w[i].l=1*new Date;
+s.parentNode.insertBefore(e,s);
+})();
+//
