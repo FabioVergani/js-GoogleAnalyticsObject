d613e5549f65b01856d3069bf4d815a77ff3d3b7
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-11-20 20:08:02 +0100

    Update GoogleAnalyticsObject.js

--- GoogleAnalyticsObject.js
+++ GoogleAnalyticsObject.js
@@ -7,7 +7,7 @@ s=d.getElementsByTagName(t)[0],
 a=(e.async=1,e.src='https://www.google-analytics.com/analytics.js');
 w[i]=w[i]||function(){
  var o=w,p=i;
- (o[p].q=o[p].q||[]).push(w,d,t,a,i,e,s);
+ (o[p].q=o[p].q||[]).push(arguments);
 };
 w[i].l=1*new Date;
 s.parentNode.insertBefore(e,s);
