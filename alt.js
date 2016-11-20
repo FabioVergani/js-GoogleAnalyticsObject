e4f06931de293bc3c0d8ccf2eb788f479b6e8192
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-11-20 20:16:15 +0100

    Create alt.js

---
+++ alt.js
@@ -0,0 +1,8 @@
+ if(i=$run.dataset.ua){
+	w.GoogleAnalyticsObject='ga';
+	l=d.createElement('script');
+	l.async=1;
+	l.src='https://www.google-analytics.com/analytics.js';
+	(w.ga=w.ga||function(){(w.ga.q=w.ga.q||[]).push(arguments)}).l=1*new Date;
+	$run.parentNode.insertBefore(l,$run);
+ };
